2ce8a2138548a534ffa9b5a4b85cbaed93792b55
Janus Reith <janus.reith@googlemail.com>
2019-07-23 19:59:09 +0200

    prebuild

--- src/utils/graphQL.js
+++ src/utils/graphQL.js
@@ -1,16 +1,14 @@
-const fetchGraphQL = (graphQLUrl, url, credentials, app, query) => {
-	//const fetchUrl = credentials ? url.replace('//', `//${credentials}@`) : url;
-
-	const mSearchBody = query.map(item => JSON.stringify(item));
+const fetchGraphQL = (graphQLUrl, url, credentials, app, mSearchQuery) => {
+	const mSearchBody = mSearchQuery.map(item => JSON.stringify(item));
 
 	const query = {
 		elastic72: {
 			msearch: {
 				index: app,
-				body: mSearchBody
-			}
-		}
-	}
+				body: mSearchBody,
+			},
+		},
+	};
 
 	return fetch(graphQLUrl, {
 		method: 'POST',
